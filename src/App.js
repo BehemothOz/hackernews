6b361ac840511e79310f17153b7178a1cabc3395
behemothoz <behemothoz@yandex.ru>
2018-07-25 18:24:44 +0300

    new style

--- src/App.js
+++ src/App.js
@@ -66,8 +66,10 @@ class App extends Component {
     const { searchTerm, list } = this.state;
 
     return (
-      <div className="App">
-        <Search value={searchTerm} onChange={this.onSearchChange}>Поиск</Search>
+      <div className="page">
+        <div className="interactions">
+          <Search value={searchTerm} onChange={this.onSearchChange}>Поиск</Search>
+        </div>
         <Table list={list} pattern={searchTerm} onDismiss={this.onDismiss}/>
       </div>
     );
@@ -85,28 +87,43 @@ const Search = ({ value, onChange, children }) =>
     />
   </form>;
 
-const Table = ({ list, pattern, onDismiss }) =>
-  <div>
-    {list.filter(isSearched(pattern)).map(item =>
-      <div key={item.objectID}>
-        <span>
-          <a href={item.url}>{item.title}</a>
-        </span>
-        <span>{item.author}</span>
-        <span>{item.num_comments}</span>
-        <span>{item.points}</span>
-        <span>
-          {/* <button
-            onClick={() => onDismiss(item.objectID)}
-            type="button"
-          >
-            Отбросить
-          </button> */}
-          <Button onClick={() => onDismiss(item.objectID)}>Отбросить</Button>
-        </span>
-      </div>
-    )}
-  </div>;
+const Table = ({ list, pattern, onDismiss }) => {
+  const largeColumn = {
+    width: '40%',
+  };
+
+  const midColumn = {
+    width: '30%',
+  };
+
+  const smallColumn = {
+    width: '10%',
+  };
+
+  return (
+    <div className="table">
+      {list.filter(isSearched(pattern)).map(item =>
+        <div key={item.objectID} className="table-row">
+          <span>
+            <a href={item.url} style={largeColumn}>{item.title}</a>
+          </span>
+          <span style={midColumn}>{item.author}</span>
+          <span style={smallColumn}>{item.num_comments}</span>
+          <span style={smallColumn}>{item.points}</span>
+          <span style={smallColumn}>
+            {/* <button
+              onClick={() => onDismiss(item.objectID)}
+              type="button"
+            >
+              Отбросить
+            </button> */}
+            <Button onClick={() => onDismiss(item.objectID)} className="button-inline">Отбросить</Button>
+          </span>
+        </div>
+      )}
+    </div>
+  );
+}
 
 const Button = ({ onClick, className = '', children }) =>
   <button
