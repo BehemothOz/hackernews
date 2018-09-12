403180d4ada85b98df5c0414adfa908971b43c20
behemothoz <behemothoz@yandex.ru>
2018-09-12 14:01:52 +0300

    chapters: state, data

--- src/App.js
+++ src/App.js
@@ -1,138 +1,61 @@
 import React, { Component } from 'react';
-// import logo from './logo.svg';
 import './App.css';
 
-const DEFAULT_QUERY = 'redux';
-
-const PATH_BASE = 'https://hn.algolia.com/api/v1';
-const PATH_SEARCH = '/search';
-const PARAM_SEARCH = 'query=';
-
-function isSearched(searchTerm) { // TEST!
-  return function(item) {
-    // условие, возвращающее true или false
-    return item.title.toLowerCase().includes(searchTerm.toLowerCase());
+const list = [
+  {
+    title: 'React',
+    url: 'https://reactjs.org/',
+    author: 'Jordan Walke',
+    num_comments: 3,
+    points: 4,
+    objectID: 0,
+  },
+  {
+    title: 'Repeat React',
+    url: 'https://reactjs.org/',
+    author: 'Jordan Walke',
+    num_comments: 3,
+    points: 4,
+    objectID: 1,
   }
-}
+];
 
 class App extends Component {
-
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      // list,
-      searchTerm: '',
-
-      result: null,
-      searchTerm: DEFAULT_QUERY,
-    }
-
-    this.onDismiss = this.onDismiss.bind(this);
-    this.onSearchChange = this.onSearchChange.bind(this);
-    this.setSearchTopStories = this.setSearchTopStories.bind(this);
+  state = {
+    list
   }
 
-  setSearchTopStories(result) {
-    this.setState({ result });
-  }
+  onDismiss = (id) => () => {
+    const { list } = this.state;
+    const updatedList = list.filter(item => item.objectID !== id);
 
-  componentDidMount() {
-    const { searchTerm } = this.state;
-
-    fetch(`${PATH_BASE}${PATH_SEARCH}?${PARAM_SEARCH}${searchTerm}`)
-      .then(response => response.json())
-      .then(result => this.setSearchTopStories(result))
-      .catch(error => error);
-  }
-
-  onDismiss(id) {
-    const isNotId = item => item.objectID !== id;
-    const updatedHits = this.state.result.hits.filter(isNotId);
-
-    // this.setState({ result: Object.assign({}, this.state.result, { hits: updatedHits }) }); // 1 способ
-    this.setState({ result: { ...this.state.result, hits: updatedHits } }); // 2 способ
-  }
-
-  onSearchChange(event) {
-    this.setState({ searchTerm: event.target.value });
+    this.setState({ list: updatedList });
   }
 
   render() {
-    const { searchTerm, result } = this.state; // ПОтестить
-    console.log(result)
-
-    // if (!result) { return null; }
-
     return (
-      <div className="page">
-        <div className="interactions">
-          <Search value={searchTerm} onChange={this.onSearchChange}>Поиск</Search>
-        </div>
-        {result && <Table list={result.hits} pattern={searchTerm} onDismiss={this.onDismiss}/>}
-        {/* {result ? <Table/> : null} - 2 вариант условной отрисовки*/}
+      <div className="App">
+        {this.state.list.map(item =>
+          <div key={item.objectID}>
+            <span>
+              <a href={item.url}>{item.title}</a>
+            </span>
+            <span>{item.author}</span>
+            <span>{item.num_comments}</span>
+            <span>{item.points}</span>
+            <span>
+              <button
+                onClick={this.onDismiss(item.objectID)}
+                type="button"
+              >
+                Отбросить
+              </button>
+            </span>
+          </div>
+        )}
       </div>
-    );
+    )
   }
 }
-
-const Search = ({ value, onChange, children }) =>
-  // const { value, onChange, children } = props; - переносим деструкт в параметры функции
-  <form>
-    <input
-      type="text"
-      value={value}
-      onChange={onChange}
-      placeholder={children}
-    />
-  </form>;
-
-const Table = ({ list, pattern, onDismiss }) => {
-  const largeColumn = {
-    width: '40%',
-  };
-
-  const midColumn = {
-    width: '30%',
-  };
-
-  const smallColumn = {
-    width: '10%',
-  };
-
-  return (
-    <div className="table">
-      {list.filter(isSearched(pattern)).map(item =>
-        <div key={item.objectID} className="table-row">
-          <span>
-            <a href={item.url} style={largeColumn}>{item.title}</a>
-          </span>
-          <span style={midColumn}>{item.author}</span>
-          <span style={smallColumn}>{item.num_comments}</span>
-          <span style={smallColumn}>{item.points}</span>
-          <span style={smallColumn}>
-            {/* <button
-              onClick={() => onDismiss(item.objectID)}
-              type="button"
-            >
-              Отбросить
-            </button> */}
-            <Button onClick={() => onDismiss(item.objectID)} className="button-inline">Отбросить</Button>
-          </span>
-        </div>
-      )}
-    </div>
-  );
-}
-
-const Button = ({ onClick, className = '', children }) =>
-  <button
-    onClick={onClick}
-    className={className}
-    type="button"
-  >
-    {children}
-  </button>
-
 
 export default App;
